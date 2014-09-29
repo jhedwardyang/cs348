e23331689e5aad55b57d00bbf9daead03795519c
Edward Yang <jh.edwardyang@gmail.com>
2014-09-29 15:00:40 -0400

    Q8 Small Mistake, LateX Fix

--- a01/Q8.sql
+++ a01/Q8.sql
@@ -2,7 +2,7 @@
 # A01 Q08
 # For each nation, list the nation key, nation name, the number of orders placed by customers living in that nation, and the cumulative price for all of those orders cast as a DECIMAL(11,2). Order the result by descending cumulative order price.
 
-SELECT n.n_nationkey n_key, n.n_name, COUNT(t.t_count) num_orders, SUM(t.t_totalprice) cum_price # select nation key, nation name, count of orders, total cumulative price of orders
+SELECT n.n_nationkey n_key, n.n_name, SUM(t.t_count) num_orders, SUM(t.t_totalprice) cum_price # select nation key, nation name, count of orders, total cumulative price of orders
 FROM ( # from a temp table
 	SELECT c.c_custkey t_custkey, COUNT(o.o_totalprice) t_count, CAST(SUM(o.o_totalprice) AS DECIMAL(11,2)) t_totalprice, c.c_nationkey t_nationkey # which returns customerkey, number of orders and price which that customer spent and which nation they are from
 	FROM ORDERS o # to get this from orders table (alias o)
